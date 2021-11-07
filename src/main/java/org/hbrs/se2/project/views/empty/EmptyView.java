68067e174c782d20afc46a7b55aa4770e01cc65d
start.vaadin.com <no-reply@vaadin.com>
2021-11-07 21:19:04 +0000

    Generated project

---
+++ src/main/java/org/hbrs/se2/project/views/empty/EmptyView.java
@@ -0,0 +1,36 @@
+package org.hbrs.se2.project.views.empty;
+
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.server.PWA;
+import com.vaadin.flow.theme.Theme;
+import com.vaadin.flow.router.RouteAlias;
+
+@PWA(name = "Coll", shortName = "Coll", enableInstallPrompt = false)
+@Theme(themeFolder = "coll")
+@PageTitle("Empty")
+@Route(value = "empty")
+@RouteAlias(value = "")
+public class EmptyView extends VerticalLayout {
+
+    public EmptyView() {
+        setSpacing(false);
+
+        Image img = new Image("images/empty-plant.png", "placeholder plant");
+        img.setWidth("200px");
+        add(img);
+
+        add(new H2("This place intentionally left empty"));
+        add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
+
+        setSizeFull();
+        setJustifyContentMode(JustifyContentMode.CENTER);
+        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
+        getStyle().set("text-align", "center");
+    }
+
+}
